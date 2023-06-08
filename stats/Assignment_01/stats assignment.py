c1985662df33c9b31be3999910da08c3a126969a
Shilu <shilu842003@gmail.com>
2023-06-08 08:25:21 +0530

    added stats assignment 01 pdf

---
+++ stats/Assignment_01/stats assignment.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ##                                               Stats Assignment 01
+
+# **1. Harvard Law School courses often have assigned seating to facilitate the “Socratic method.”
+# Suppose that there are 100 first year Harvard Law students, and each takes two courses:
+# Torts and Contracts. Both are held in the same lecture hall (which has 100 seats), and the
+# seating is uniformly random and independent for the two courses.**
+
+# **(a) Find the probability that no one has the same seat for both courses (exactly; you should
+# leave your answer as a sum).**
+
+# <img src='Images/Harvard_ques_a_answer.jpg' />
+
+# **(b) Find a simple but accurate approximation to the probability that no one has the same
+# seat for both courses.**
+
+# <img src='Images/Harvard_ques_b_answer.jpg' />
+
+# **(c) Find a simple but accurate approximation to the probability that at least two students
+# have the same seat for both courses.**
+
+# <img src='Images/Harvard_ques_c_answer.jpg' />
+
+# In[ ]:
+
+
+
+
